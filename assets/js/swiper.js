43e02f2c7e7a21af254480fc5ec9fbaa8630cc60
Limon1980 <tarkalmob@yandex.ru>
2020-05-12 23:03:32 +1000

    slider+

---
+++ assets/js/swiper.js
@@ -0,0 +1,8 @@
+new Swiper(".swiper-container", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  //     slidesPerView: 1,
+  //    slidesPerColumn: 1
+});
--- index.php
+++ index.php
@@ -14,43 +14,47 @@ Template Post Type: post, page, product
 
 		   <ul class="slides">
 
+         
+      <?php 
+  global $wpdb3;
+
+include 'const.php';
+  
+  // если не удалось подключиться, и нужно оборвать PHP с сообщением об этой ошибке
+  if( ! empty($wpdb3->error) )
+     wp_die( $wpdb3->error );
+  
+  // Готово, теперь используем функции класса wpdb
+  $results = $wpdb3->get_results( "SELECT link FROM vk_parser ORDER BY id DESC LIMIT 5" );
+ // var_dump($results);
+
+ foreach($results as $key){
+echo '
+  
 			   <!-- Slide -->
 			   <li>
 				   <div class="row">
 					   <div class="twelve columns">
 						   <div class="slider-text">
-							   <h1>Free amazing site template<span>.</span></h1>
+                        <h1>Free amazing site template<span>.</span></h1>
+                        <div class="slider-image">
+                        <img src="'.$key->link.'" alt="" width=30%/>
+                     </div>
 							   <p>Aenean condimentum, lacus sit amet luctus lobortis, dolores et quas molestias excepturi
                         enim tellus ultrices elit, amet consequat enim elit noneas sit amet luctu. lacus sit amet luctus lobortis, dolores et quas molestias excepturi
                         enim tellus ultrices elit.</p>
 						   </div>
-                     <div class="slider-image">
-                        <img src="images/sliders/home-slider-image-01.png" alt="" />
-                     </div>
+                   
 					   </div>
 				   </div>
 			   </li>
+            ';
 
-            <!-- Slide -->
-			   <li>
-				   <div class="row">
-					   <div class="twelve columns">
-						   <div class="slider-text">
-							   <h1>Responsive + HTML5 + CSS3<span>.</span></h1>
-							   <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
-                        deleniti eos et accusamus. amet consequat enim elit noneas sit amet luctu. lacus sit amet luctus lobortis.
-                        Aenean condimentum, lacus sit amet luctus.</p>
-						   </div>
-                     <div class="slider-image">
-                        <img src="images/sliders/home-slider-image-02.png" alt="" />
-                     </div>
-					   </div>
-				   </div>
-			   </li>
+            }
 
-		   </ul>
+            
+                    ?>
 
-	   </div> <!-- Flexslider End-->
 
    </section> <!-- Intro Section End-->
 
@@ -186,6 +190,7 @@ Template Post Type: post, page, product
 
       <div class="blog-entries">
 
+
                 <?php 
                 // параметры по умолчанию
         $posts = get_posts( array(
@@ -201,6 +206,7 @@ Template Post Type: post, page, product
             // формат вывода the_title() ...
 
             ?>
+
                    <!-- Entry -->
          <article class="row entry">
 
@@ -244,6 +250,8 @@ Template Post Type: post, page, product
       </div> <!-- Entries End -->
 
    </section> <!-- Journal Section End-->
+
+
 
    <!-- Call-To-Action Section
    ================================================== -->
